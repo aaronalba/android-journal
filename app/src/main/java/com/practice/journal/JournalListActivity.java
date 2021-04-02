b194a0e182f063f1e150ebb8af1bace2138c4bf1
aaronalba <aaalba1@up.edu.ph>
2021-04-02 08:43:33 +0800

    ADD JournalListActivity class
    This Activity class will show a list of entries made in the Journal.

---
+++ app/src/main/java/com/practice/journal/JournalListActivity.java
@@ -0,0 +1,17 @@
+/**
+ * Activity class for showing a list of entries in the journal.
+ * @author Aaron Alba
+ */
+
+package com.practice.journal;
+
+import androidx.fragment.app.Fragment;
+
+public class JournalListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        // TODO: Define the fragment that will be used by this activity
+        return null;
+    }
+}
