f8cfac8b702d6e182eb779621e71aebcee367d11
aaronalba <aaalba1@up.edu.ph>
2021-04-02 13:48:33 +0800

    ADD Adapter and ViewHolder inner classes
    The JournalAdapter Class will manage the RecyclerView in the
    JournalListFragment. The JournalHolder Class defines the layout
    of the items that will be shown in the RecyclerView.

--- app/src/main/java/com/practice/journal/JournalListFragment.java
+++ app/src/main/java/com/practice/journal/JournalListFragment.java
@@ -5,15 +5,20 @@
 
 package com.practice.journal;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
 
 public class JournalListFragment extends Fragment {
     private RecyclerView mRecyclerView;
@@ -40,5 +45,66 @@ public class JournalListFragment extends Fragment {
         mRecyclerView = view.findViewById(R.id.entry_recycler_view);
 
         return view;
+    }
+
+
+
+    /*
+        Adapter Class for feeding the RecyclerView with ViewHolders from the list of entries.
+        This class creates the needed ViewHolder defined in JournalViewHolder and binds that
+        view to the Data from the List of Journal Entries, then it is shown by the RecyclerView.
+     */
+    private class JournalAdapter extends RecyclerView.Adapter<JournalHolder> {
+        private List<Entry> mList;
+
+
+        public JournalAdapter(Context context) {
+            this.mList = EntryStash.get(context).getEntries();
+        }
+
+        @NonNull
+        @Override
+        public JournalHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new JournalHolder(getLayoutInflater(), parent);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull JournalHolder holder, int position) {
+            holder.bind(mList.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return mList.size();
+        }
+    }
+
+    /*
+        ViewHolder Class that inflates the Views that will be shown by the RecyclerView.
+        This class essentially defines the views inflated in the list_item_entry.xml
+     */
+    private class JournalHolder extends RecyclerView.ViewHolder {
+        private TextView mTitleTextView;
+        private TextView mDateTextView;
+        private ImageButton mEditButton;
+        private ImageButton mDeleteButton;
+
+
+        public JournalHolder(LayoutInflater inflater, ViewGroup parent) {
+            // inflate the layout. The root view can be found in the ViewHolder member field named itemView
+            super(inflater.inflate(R.layout.list_item_entry, parent, false));
+
+            // get references to the text view
+            mTitleTextView = itemView.findViewById(R.id.entry_title);
+            mDateTextView = itemView.findViewById(R.id.entry_date);
+            mEditButton = itemView.findViewById(R.id.edit_button);
+            mDeleteButton = itemView.findViewById(R.id.delete_button);
+        }
+
+        // This method is called by the adapter to fill the data in this ViewHolder
+        public void bind(Entry entry) {
+            mTitleTextView.setText(entry.getTitle());
+            mDateTextView.setText(entry.getDate().toString());
+        }
     }
 }
