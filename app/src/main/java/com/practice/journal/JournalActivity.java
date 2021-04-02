489231d2769e8b3b3ba10c342ad7acdcf2768c4d
aaronalba <aaalba1@up.edu.ph>
2021-04-02 16:27:50 +0800

    ADD newIntent method in JournalActivity
    This newIntent method can be used to start the JournalAcitivty and to
    also ensure that the needed id paramter for its fragment will be passed
    when the activity is started.

--- app/src/main/java/com/practice/journal/JournalActivity.java
+++ app/src/main/java/com/practice/journal/JournalActivity.java
@@ -5,11 +5,40 @@
 
 package com.practice.journal;
 
+import android.content.Context;
+import android.content.Intent;
+
+
 import androidx.fragment.app.Fragment;
 
+import java.util.UUID;
+
 public class JournalActivity extends SingleFragmentActivity {
+
+    private static final String EXTRA_ENTRY_ID = "com.practice.journal.entryid";
+
+
+    /**
+     * This method creates the fragment that will be hosted by this activity. This method is used
+     * in the template class named SingleFragmentActivity.
+     * @return The fragment that this activity will host.
+     */
     @Override
     protected Fragment createFragment() {
-        return new JournalFragment();
+        // create the fragment by using its newInstance method to pass in the need UUID argument
+        return JournalFragment.newInstance((UUID) getIntent().getSerializableExtra(EXTRA_ENTRY_ID));
+    }
+
+
+    /**
+     * This creates an intent that can be used to launch this activity.
+     * @param context The activity that will launch this intent.
+     * @param id The id argument needed to create the JournalFragment.
+     * @return  The intent that can be used to launch JournalActivity.
+     */
+    public static Intent newIntent(Context context, UUID id) {
+        Intent intent = new Intent(context, JournalActivity.class);
+        intent.putExtra(EXTRA_ENTRY_ID, id);
+        return intent;
     }
 }
