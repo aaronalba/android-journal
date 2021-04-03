e7c39f54cc3de32af44826f5fdd67e618cccae03
aaronalba <aaalba1@up.edu.ph>
2021-04-03 18:22:25 +0800

    ADD ViewerFragment class
    This Fragment previews the information held by an Entry so that the user
    can read that Entry.

---
+++ app/src/main/java/com/practice/journal/ViewerFragment.java
@@ -0,0 +1,71 @@
+/**
+ * Fragment class that previews an Entry.
+ * @author Aaron Alba
+ */
+
+package com.practice.journal;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import java.util.UUID;
+
+public class ViewerFragment extends Fragment {
+    private TextView titleTextField;
+    private TextView dateTimeTextField;
+    private TextView contentTextField;
+
+    private static final String ARGS_ENTRY_ID = "uuid";
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+    /**
+     * Used in creating a ViewerFragment instance with an id argument telling which Entry in the
+     * list of entries that the fragment will preview.
+     * @param id The UUID of the Entry object.
+     * @return ViewerFragment object.
+     */
+    public static ViewerFragment newInstance(UUID id) {
+        // create the fragment arguments
+        Bundle args = new Bundle();
+        args.putSerializable(ARGS_ENTRY_ID, id);
+
+        // return a new ViewerFragment with the specified arguments
+        ViewerFragment fragment = new ViewerFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+    /**
+     * This lifecycle method inflates the layout resource that will be used by this fragment.
+     * @param inflater  the layout inflater.
+     * @param container the parent view that this fragment's UI should attach to.
+     * @param savedInstanceState the Bundle containing data from a previous state.
+     * @return the inflated view of the fragment.
+     */
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // inflate the layout
+        View view = inflater.inflate(R.layout.fragment_viewer, container, false);
+
+        // get the references to the Views in the layout
+        titleTextField = view.findViewById(R.id.title_viewer);
+        dateTimeTextField = view.findViewById(R.id.datetime_viewer);
+        contentTextField = view.findViewById(R.id.content_viewer);
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
