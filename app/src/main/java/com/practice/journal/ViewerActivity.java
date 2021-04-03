f60d80da502016ac7daf1439f1b060aa7118ed7d
aaronalba <aaalba1@up.edu.ph>
2021-04-03 18:37:18 +0800

    ADD ViewerActivity class
    This Activity shows the preview of the information contained in an
    Entry object using the ViewerFragment.

---
+++ app/src/main/java/com/practice/journal/ViewerActivity.java
@@ -0,0 +1,40 @@
+/**
+ * Activity class for previewing an Entry.
+ * @author Aaron A. Alba
+ */
+
+package com.practice.journal;
+
+import android.content.Intent;
+
+import androidx.fragment.app.Fragment;
+
+import java.util.UUID;
+
+public class ViewerActivity extends SingleFragmentActivity {
+
+    private static final String EXTRA_ENTRY_ID = "com.practice.journal.entryid";
+
+    /**
+     * Defines the fragment that will be hosted by this Activity.
+     * @return The Fragment to be hosted.
+     */
+    @Override
+    protected Fragment createFragment() {
+        // retrieve the id from the intent extra and use that id for creating the ViewerFragment
+        UUID id = (UUID) getIntent().getSerializableExtra(EXTRA_ENTRY_ID);
+        return ViewerFragment.newInstance(id);
+    }
+
+
+    /**
+     * Creates a new intent for launching this activity.
+     * @param id The intent extra which contains the UUID of the Entry that will be previewed.
+     * @return Intent object for starting this activity.
+     */
+    public Intent newIntent(UUID id) {
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_ENTRY_ID, id);
+        return intent;
+    }
+}
