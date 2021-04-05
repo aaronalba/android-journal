b38e201b1ce5179c754e95c6089d86c0fcb48378
aaronalba <aaalba1@up.edu.ph>
2021-04-05 16:54:42 +0800

    ADD formatDate method in JournalUtil Class
    This method applies a format to a Date object and returns the string
    containing the formatted date.

--- app/src/main/java/com/practice/journal/JournalUtil.java
+++ app/src/main/java/com/practice/journal/JournalUtil.java
@@ -14,10 +14,10 @@ public final class JournalUtil {
 
 
     /**
-     * Applies a format to a Date object and returns the string date.
+     * Applies a format to a Date object and returns the string date time.
      * @param date The Date object to be formatted.
      * @param isMilitaryTime Tells whether the format should be in military time or am-pm time.
-     * @return The String containing the formatted date.
+     * @return The String containing the formatted date time.
      */
     public static String formatDateTime(Date date, boolean isMilitaryTime) {
         // create the formatter
@@ -30,6 +30,21 @@ public final class JournalUtil {
         // format the date
         return format.format(date);
     }
+
+
+    /**
+     * Applies a format to a Date object and returns the string date.
+     * @param date The Date object to be formatted.
+     * @return The String containing the formatted date.
+     */
+    public static String formatDate(Date date) {
+        // create the formatter
+        SimpleDateFormat format = new SimpleDateFormat("E dd MMM yyyy");
+
+        // format the date
+        return format.format(date);
+    }
+
 
 
 }
