3b065bbd43387d44941ff885cc9c043bdfa7d9e9
aaronalba <aaalba1@up.edu.ph>
2021-05-06 19:39:55 +0800

    ADD JournalSignupFragment class

--- app/src/main/java/com/practice/journal/fragments/JournalSignupFragment.java
+++ app/src/main/java/com/practice/journal/fragments/JournalSignupFragment.java
@@ -5,6 +5,7 @@
 
 package com.practice.journal.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -12,18 +13,24 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.practice.journal.R;
+import com.practice.journal.models.User;
+import com.practice.journal.models.UserStash;
 
 public class JournalSignupFragment extends Fragment {
     private EditText mNameField;
     private EditText mPinField;
     private EditText mConfirmField;
     private Button mSignupButton;
+
+    // key for the intent extra to be sent to the starting activity
+    public static final String EXTRA_SIGNUP_STATUS = "signup_status";
 
     /**
      * Inflates the layout that will be used by this fragment. Also retrieves references to the widgets defined in that layout.
@@ -49,10 +56,56 @@ public class JournalSignupFragment extends Fragment {
 
         // get the signup button
         mSignupButton = v.findViewById(R.id.signup_button);
+        mSignupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = mNameField.getText().toString();
+                String pin = mPinField.getText().toString();
+                String confirm = mConfirmField.getText().toString();
+                User user = new User(name, pin);
+
+                // check if the name field is empty
+                if (mNameField.getText().toString().length() == 0) {
+                    Toast.makeText(getContext(), getString(R.string.toast_noname), Toast.LENGTH_SHORT).show();
+                    mNameField.requestFocus();
+                    return;
+                }
+
+                // check if the password is at least 4 digits
+                if (mPinField.getText().toString().length() < 4) {
+                    Toast.makeText(getContext(), getString(R.string.toast_pin_min_length), Toast.LENGTH_SHORT).show();
+                    mPinField.requestFocus();
+                    return;
+                }
+
+                // check user name validity
+                if (UserStash.get(getContext()).hasUser(name)) {
+                    Toast.makeText(getContext(), getString(R.string.toast_user_exist), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // if entered pin is equal to confirm pin
+                if (pin.equals(confirm)) {
+
+                    // create the user
+                    UserStash.get(getContext()).createUser(user);
+                    Toast.makeText(getContext(), getString(R.string.toast_user_created), Toast.LENGTH_SHORT).show();
+
+                    // end the activity
+                    Intent intent = new Intent();
+                    intent.putExtra(EXTRA_SIGNUP_STATUS, true);
+                    getActivity().finish();
+
+                } else {
+                    // pin not equal
+                    Toast.makeText(getContext(), getString(R.string.toast_unmatched_pin), Toast.LENGTH_SHORT).show();
+                    mPinField.setText("");
+                    mConfirmField.setText("");
+                    mPinField.requestFocus();
+                }
+            }
+        });
 
         return v;
     }
-
-
-
 }
